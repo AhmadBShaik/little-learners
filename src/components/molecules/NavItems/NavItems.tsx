a95140f32102f1fe650e2ea0ab768e7a4c0929aa
ahmad-bsh <ahmad.bsh.develops@gmail.com>
2024-07-19 22:11:34 +0530

    refactor: render  nav items from array

--- src/components/molecules/NavItems/NavItems.tsx
+++ src/components/molecules/NavItems/NavItems.tsx
@@ -1,26 +1,31 @@
-import Logo from "@/components/atoms/Logo";
 import NavItem from "@/components/atoms/NavItem";
 import Menu from "@/icons/Menu";
-import React from "react";
 
 type Props = {};
 
 function NavItems({}: Props) {
-  const navItems = [{ href: "/", children: "Home" }];
+  const navItems = [
+    { link: "/", name: "Home" },
+    { link: "/about-us", name: "About Us" },
+    { link: "/academics", name: "Academics" },
+    { link: "/admissions", name: "Admissions" },
+    { link: "/student-life", name: "Student Life" },
+  ];
   return (
     <>
       <div className="hidden divide-x-2 divide-secondary-15 border-l-2 border-secondary-15 leading-[150%] laptop:block">
-        <NavItem href={"/"}>Home</NavItem>
-        <NavItem href={"/about-us"}>About Us</NavItem>
-        <NavItem href={"/academics"}>Academics</NavItem>
-        <NavItem href={"/admissions"}>Admissions</NavItem>
-        <NavItem href={"/student-life"}>Student Life</NavItem>
-        <NavItem
-          href={"/contact"}
-          className="bg-primary-75 hover:bg-primary-70"
-        >
-          Contact
-        </NavItem>
+        {navItems.map((item, index) => (
+          <NavItem
+            href={item.link}
+            className={
+              index === navItems.length - 1
+                ? "bg-primary-75 hover:bg-primary-70"
+                : ""
+            }
+          >
+            {item.name}
+          </NavItem>
+        ))}
       </div>
       <div className="bg-primary-95 px-[30px] py-[17px] laptop:hidden">
         <Menu />
